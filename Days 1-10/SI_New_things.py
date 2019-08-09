ecbc951be7b4fd99909efd75253a800a77335f65
Chaitanya Vankadaru <chaitanya.vankadaru@gmail.com>
2019-08-09 20:29:56 +0530

    Things I felt worth sharing about Python.
    The things I have put up here are a compilation of things I felt worth sharing and they surely are tricky and interesting to know about.

--- Days 1-10/SI_New_things.py
+++ Days 1-10/SI_New_things.py
@@ -34,3 +34,23 @@ print(type(tuple1),type(tuple2))
 # Output: <class 'tuple'> <class 'int'>
 # In tuple1 we used a comma to explicitly define it as a tuple, but in the tuple2 Python Intrepreter considered it to be a 'int' datatype value.
 
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
+## Now onwards to sets and dictionaries.
+# Since, we know both of them have same syntax braces, there are some things to take care of when we are dealing with both of them.
+
+# I want to create a empty set and a empty dictionary. So, how do we do it?
+
+set1={}
+dict1={} # Is this correct?
+print(type(set1),type(dict1))
+
+# Output: <class 'dict'> <class 'dict'>
+# Which means it's not the right way to do it, because Python intrepreter considers any {} assigned to valid variable as a dictionary by default.
+
+# So, is this it? Can't we create a empty set? There is a way to do it as well.
+set2 = set()
+print(type(set2))
+
+# Output: <class 'set'> # We did it, yay!
+# So, that's how we can create empty sets without issues.
